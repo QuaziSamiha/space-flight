9263c20fca47c1c07b798e94300c822cca5fb7f8
Quazi Samiha <quazisamiha@gmail.com>
2023-10-21 09:36:08 -0700

    home component css done

--- src/components/Pages/Home.tsx
+++ src/components/Pages/Home.tsx
@@ -6,10 +6,10 @@ function Home() {
     <>
       <div>
         <div className="mx-auto  my-16">
-          <h3 className="my-4 text-left sm:text-center text-[#212529] font-medium text-3xl leading-8">
+          <h3 className="my-2 text-left sm:text-center text-[#212529] font-medium text-3xl leading-8">
             Spaceflight Details
           </h3>
-          <p className="my-6 text-left sm:text-center text-[#495057] font-normal text-base leading-6">
+          <p className="my-2 text-left sm:text-center text-[#495057] font-normal text-base leading-6">
             <small>
               Find out the elaborate features of all the past big spaceflights
             </small>
